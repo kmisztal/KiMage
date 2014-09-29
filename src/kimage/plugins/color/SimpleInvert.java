e09c352d0b06f42a0253a3f36a6bf76f4c9677d8
Krzysztof <krzysztof.misztal@gmail.com>
2014-09-29 10:52:44 +0200

    simple invert test without LUT

---
+++ src/kimage/plugins/color/SimpleInvert.java
@@ -0,0 +1,25 @@
+package kimage.plugins.color;
+
+import kimage.image.Image;
+import kimage.plugins.Plugin;
+import kimage.utils.Attributes;
+import kimage.utils.Mask;
+
+public class SimpleInvert extends Plugin {
+
+    @Override
+    public void process(Image imgIn, Image imgOut, Attributes attrOut, Mask mask) {
+        final int height = imgIn.getHeight();
+        final int widht = imgIn.getWidth();
+        
+        for (int x = 0; x < widht; x++) {
+            for (int y = 0; y < height; y++) {
+                final int red = imgIn.getRed(x, y);
+                final int green = imgIn.getGreen(x, y);
+                final int blue = imgIn.getBlue(x, y);
+                
+                imgOut.setRGB(x, y, 255 - red, 255 - green, 255 - blue);
+            }
+        }
+    }
+}
---
+++ src/test/PluginTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import kimage.image.Image;
+import kimage.plugins.Plugin;
+import kimage.plugins.color.SimpleInvert;
+import kimage.utils.gui.ImageFrame;
+
+/**
+ *
+ * @author Krzysztof
+ */
+public class PluginTest {
+    public static void main(String[] args) {
+        // load file
+        String filename = "./res/tucano.jpg";
+        Image image = new Image(filename);
+
+        //display image
+        ImageFrame before = new ImageFrame(image);
+        before.display();
+        
+        //Load plugin
+        Plugin plugin = new SimpleInvert();
+        plugin.process(image, image);
+        
+        //display image
+        ImageFrame after = new ImageFrame(image);
+        after.display();
+        
+        //save results
+        image.save("./res/out.png");
+    }
+}
