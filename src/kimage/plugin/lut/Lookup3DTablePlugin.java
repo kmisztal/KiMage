42a50da9747514e11db2eba864cab8cc38e3b22f
Krzysztof <krzysztof.misztal@gmail.com>
2015-01-10 23:32:33 +0100

    Sepia & LUT for 3D

--- src/kimage/plugin/lut/Lookup3DTablePlugin.java
+++ src/kimage/plugin/lut/Lookup3DTablePlugin.java
@@ -4,28 +4,30 @@ import java.awt.image.BufferedImageOp;
 import java.awt.image.LookupOp;
 import java.awt.image.ShortLookupTable;
 import kimage.image.Image;
-import kimage.plugin.Plugin;
-import kimage.plugin.extras.Attributes;
-import kimage.plugin.extras.Mask;
+import kimage.plugin.simple.SimplePlugin;
 
 /**
  * Lookup Table filter with different table for each channel
  *
  * @author Krzysztof
  */
-public abstract class Lookup3DTablePlugin extends Plugin {
+public abstract class Lookup3DTablePlugin extends SimplePlugin {
 
-    protected short[][] LUT;
+    protected short[] red = new short[256];
+    protected short[] green = new short[256];
+    protected short[] blue = new short[256];
 
+    /**
+     * method should fill arrays: red, green, blue
+     */
     protected abstract void createLUT();
 
     @Override
-    public void process(Image imgIn, Image imgOut, Attributes attrOut, Mask mask) {
-        if (mask != null) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
+    public void process(Image imgIn, Image imgOut) {
         createLUT();
+        short[][] LUT = new short[][]{
+            red, green, blue
+        };
 
         final BufferedImageOp bio
                 = new LookupOp(new ShortLookupTable(0, LUT), null);
