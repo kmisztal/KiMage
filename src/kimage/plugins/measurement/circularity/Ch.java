8c372b16f65e35528319d399e435dc00e002fe59
kmisztal <krzysztof.misztal@gmail.com>
2015-08-11 18:26:04 +0200

    ShapeMeasuremenst by M. Chołoniewski

---
+++ src/kimage/plugins/measurement/circularity/Ch.java
@@ -0,0 +1,22 @@
+package kimage.plugins.measurement.circularity;
+
+import kimage.image.Image;
+import kimage.plugin.measurement.ShapeMeasurement;
+
+/**
+ *
+ * @author Marcin Chołoniewski
+ */
+public class Ch extends ShapeMeasurement {
+    
+    @Override
+    public void process(Image imgIn, Image imgOut) {
+        final long array = getArray(imgIn);
+        final double circ = (array * array) /
+                (2.0 * Math.PI *
+                (getCentralMoment(2,0,imgIn) + getCentralMoment(0,2,imgIn)));
+        
+        setAttribute("circularity", round(circ, 4));
+    }
+    
+}
---
+++ src/kimage/plugins/measurement/ellipticity/EpsH.java
@@ -0,0 +1,32 @@
+package kimage.plugins.measurement.ellipticity;
+
+import kimage.image.Image;
+import kimage.plugin.measurement.ShapeMeasurement;
+
+/**
+ *
+ * @author Marcin Chołoniewski
+ */
+public class EpsH extends ShapeMeasurement {
+
+    @Override
+    public void process(Image imgIn, Image imgOut) {
+        
+        final double n20 = getNormalizedMoment(2, 0, imgIn);
+        final double n02 = getNormalizedMoment(0, 2, imgIn);
+        final double n11 = getNormalizedMoment(1, 1, imgIn);
+
+        final double inv1 = n20 + n02;
+        final double inv2 = Math.pow((n20 - n02), 2) + (4 * Math.pow(n11, 2));       
+
+        final double ellip = 1 / (2 * Math.pow(Math.PI,2) * 
+                (inv1 * 
+                Math.sqrt(4 * inv2 + 1 / Math.pow(Math.PI, 2) ) -
+                2 * inv2));
+
+        setAttribute("ellipticity", round(ellip, 4));
+                
+    }
+    
+}
+
