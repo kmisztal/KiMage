c50d7d92e96963029f174f05705f1acdb7ea8f6b
kmisztal <krzysztof.misztal@gmail.com>
2015-09-05 08:51:30 +0200

    same approach to concurrency in plugins application to images cont.

--- src/sample/use/gui/GUITest.java
+++ src/sample/use/gui/GUITest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sample.use;
+package sample.use.gui;
 
 import kimage.image.Image;
 import kimage.utils.gui.ImageFrame;
