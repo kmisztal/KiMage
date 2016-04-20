630a7e11b71d3d0184dcd778e84d012e8bf7b8bc
Damian Krawiec <dbgbkr@gmail.com>
2016-04-20 23:21:06 +0200

    [#22] Add Task command

--- imageprocessor/imageprocessor-api/src/main/java/pl/edu/uj/kimage/api/TaskCommand.java
+++ imageprocessor/imageprocessor-api/src/main/java/pl/edu/uj/kimage/api/TaskCommand.java
@@ -1,6 +1,19 @@
 package pl.edu.uj.kimage.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import pl.edu.uj.kimage.eventbus.Command;
 
+/**
+ * Command with task which is handled by image processor
+ */
 public class TaskCommand implements Command {
+    private final Task task;
+
+    public TaskCommand(@JsonProperty("task") Task task) {
+        this.task = task;
+    }
+
+    public Task getTask() {
+        return task;
+    }
 }
