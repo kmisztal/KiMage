e7bfcef775c658912c5985060f786de34f5db15c
annadobrowolska <anna90dobrowolska@gmail.com>
2016-05-18 21:21:17 +0200

    [#25] Configuration auto reload

--- configuration/src/main/java/pl/edu/uj/kimage/configuration/ConfigurationReader.java
+++ configuration/src/main/java/pl/edu/uj/kimage/configuration/ConfigurationReader.java
@@ -1,16 +1,11 @@
 package pl.edu.uj.kimage.configuration;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 
 import javax.annotation.concurrent.ThreadSafe;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 
 @ThreadSafe
@@ -20,21 +15,20 @@ public class ConfigurationReader {
     private static final String SEPARATOR = "=";
     private static ConfigurationReader INSTANCE;
     private static ImmutableMap<String, String> SETTINGS;
-    private static String PATH;
 
     private ConfigurationReader(String path) {
         SETTINGS = loadConfig(path);
+        registerWatchService(path);
     }
 
     /**
      * @param path path to properties file in resource folder
      */
     public static ConfigurationReader getInstance(String path) {
-        PATH = path;
         if (INSTANCE == null) {
             synchronized (ConfigurationReader.class) {
                 if (INSTANCE == null) {
-                    INSTANCE = new ConfigurationReader(PATH);
+                    INSTANCE = new ConfigurationReader(path);
                 }
             }
         }
@@ -57,38 +51,31 @@ public class ConfigurationReader {
         }
     }
 
-    public synchronized void update(String settingName, String newValue) {
-        List<String> propertiesLines = Lists.newArrayList();
-        ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
-        ImmutableMap<String, String> configToUpdate = loadConfig(PATH);
-        ImmutableSet<String> settingsSet = configToUpdate.keySet();
-        if (settingsSet.size() == 0) {
-            builder.put(settingName, newValue);
-            propertiesLines.add(settingName + SEPARATOR + newValue);
-        } else {
-            for (String name : settingsSet) {
-                if (name.equals(settingName)) {
-                    builder.put(settingName, newValue);
-                    propertiesLines.add(settingName + SEPARATOR + newValue);
-                } else {
-                    String value = SETTINGS.get(name);
-                    builder.put(name, value);
-                    propertiesLines.add(name + SEPARATOR + newValue);
+    private void registerWatchService(String pathToFile) {
+        Runnable runnable = () -> {
+            try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
+                Path path1 = Paths.get(ClassLoader.getSystemResource(pathToFile).toURI()).getParent();
+                path1.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+
+                while (true) {
+                    WatchKey key = watchService.take();
+                    for (WatchEvent<?> event : key.pollEvents()) {
+                        Path changed = (Path) event.context();
+                        if (changed.endsWith(pathToFile)) {
+                            // TODO logging
+                            loadConfig(pathToFile);
+                        }
+                    }
+                    boolean valid = key.reset();
+                    if (!valid) {
+                        break;
+                    }
                 }
+            } catch (IOException | InterruptedException | URISyntaxException e) {
+                throw new IllegalStateException(e);
             }
-        }
-
-        SETTINGS = builder.build();
-        updateConfig(PATH, propertiesLines);
-    }
-
-    void updateConfig(String fileName, List<String> properties) {
-        try {
-            Path path = Paths.get(ClassLoader.getSystemResource(fileName).toURI());
-            Files.write(path, properties, StandardCharsets.UTF_8);
-        } catch (URISyntaxException | IOException e) {
-            throw new IllegalStateException(e);
-        }
+        };
+        new Thread(runnable).start();
     }
 
     public String read(String settingName) {
--- configuration/src/test/java/pl/edu/uj/kimage/configuration/ConfigurationReaderTest.java
+++ configuration/src/test/java/pl/edu/uj/kimage/configuration/ConfigurationReaderTest.java
@@ -1,21 +1,11 @@
 package pl.edu.uj.kimage.configuration;
 
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
 import org.junit.Test;
-
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 public class ConfigurationReaderTest {
 
     private static final String PATH = "testConfig.properties";
-    private static final List<String> VALID_CONFIG_FIRST_LINE = Lists.newArrayList("NUMBER_OF_THREADS=2");
-    private static final String UPDATED_CONFIG_FIRST_LINE = "NUMBER_OF_THREADS=newValue";
-    private static final String UPDATED_CONFIG_FIRST_LINE_VALUE = "newValue";
     private static final String VALID_PROPERTY_NAME = "NUMBER_OF_THREADS";
 
     @Test
@@ -23,42 +13,5 @@ public class ConfigurationReaderTest {
         // all properties are stored as text
         ConfigurationReader configurationReader = ConfigurationReader.getInstance(PATH);
         configurationReader.read(VALID_PROPERTY_NAME);
-    }
-
-    @Test
-    public void updateCachedPropertiesWithoutExceptions() {
-        // given
-        ConfigurationReader configurationReader = ConfigurationReader.getInstance(PATH);
-        String oldValue = configurationReader.read(VALID_PROPERTY_NAME);
-        String newValue = "newValue";
-
-        // when
-        configurationReader.update(VALID_PROPERTY_NAME, newValue);
-
-        // then
-        String updatedValue = configurationReader.read(VALID_PROPERTY_NAME);
-        configurationReader.update(VALID_PROPERTY_NAME, oldValue);
-
-        assertEquals(newValue, updatedValue);
-    }
-
-    @Test
-    public void updateConfigFileWithoutExceptions() {
-        // given
-        ConfigurationReader configurationReader = ConfigurationReader.getInstance(PATH);
-        ImmutableMap<String, String> properties = configurationReader.loadConfig(PATH);
-        String oldValue = properties.get(VALID_PROPERTY_NAME);
-        List<String> newProperties = Lists.newArrayList(UPDATED_CONFIG_FIRST_LINE);
-
-        // when
-        configurationReader.updateConfig(PATH, newProperties);
-        ImmutableMap<String, String> updatedProperties = configurationReader.loadConfig(PATH);
-
-        // then
-        String updatedProperty = updatedProperties.get(VALID_PROPERTY_NAME);
-        configurationReader.updateConfig(PATH, VALID_CONFIG_FIRST_LINE);
-
-        assertEquals(UPDATED_CONFIG_FIRST_LINE_VALUE, updatedProperty);
-        assertNotEquals(oldValue, updatedProperty);
     }
 }
