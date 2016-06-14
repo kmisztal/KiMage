cd731b134bb2f7f3f5c1e2d0e9566df0b262435a
Łukasz <lukasz.nidecki@student.uj.edu.pl>
2016-06-14 11:16:19 +0200

    gaussian-noise plugin added and corrected

--- plugins/lookup-table/src/main/java/pl/edu/uj/kimage/plugins/lookup/table/GaussianNoiseFlowStep.java
+++ plugins/lookup-table/src/main/java/pl/edu/uj/kimage/plugins/lookup/table/GaussianNoiseFlowStep.java
@@ -1,7 +1,65 @@
 package pl.edu.uj.kimage.plugins.lookup.table;
 
-/**
- * Created by lnide on 6/14/2016.
- */
-public class GaussianNoiseFlowStep {
+import pl.edu.uj.kimage.api.Step;
+import pl.edu.uj.kimage.api.StepDependency;
+import pl.edu.uj.kimage.eventbus.EventBus;
+import pl.edu.uj.kimage.plugin.FlowStep;
+import pl.edu.uj.kimage.plugin.ImageCalculated;
+import pl.edu.uj.kimage.plugin.StepResultEvent;
+import pl.edu.uj.kimage.plugin.model.Color;
+import pl.edu.uj.kimage.plugin.model.Image;
+import pl.edu.uj.kimage.plugin.model.ImageBuilder;
+
+import java.util.List;
+
+public class GaussianNoiseFlowStep extends FlowStep {
+
+    public GaussianNoiseFlowStep(Step step, EventBus eventBus) {
+        super(step, eventBus);
+    }
+
+    @Override
+    public void processRelatedEvent(StepResultEvent event) {
+        List<StepDependency> dependencies = getStep().getDependencies();
+        StepDependency stepDependency = dependencies.get(0);
+        String eventTypeName = stepDependency.getEventTypeName();
+
+        try {
+            Class<?> clazz = Class.forName(eventTypeName);
+            if (clazz.isInstance(ImageCalculated.class)) {
+
+                ImageCalculated imageCalculated = (ImageCalculated) event;
+                Image input = imageCalculated.getLoadedImage();
+
+                double p = 0.1;
+                double sigma = 10;
+
+                int height = input.getHeight();
+                int width = input.getWidth();
+
+                ImageBuilder builder = new ImageBuilder(width, height);
+                for (int yCounter = 0; yCounter < height; yCounter++) {
+                    for (int xCounter = 0; xCounter < width; xCounter++) {
+
+                        if (Math.random() < p) {
+                            double u1 = Math.random();
+                            double u2 = Math.random();
+                            int v = (int) (sigma * Math.sqrt(-2. * Math.log(u1)) * Math.cos(2. * Math.PI * u2));
+
+                            Color color = input.getColor(xCounter, yCounter);
+
+                            Color newColor = new Color(color.getRed() + v, color.getGreen() + v, color.getBlue() + v);
+                            builder.withColor(newColor, xCounter, yCounter);
+                        }
+                    }
+                }
+
+                Image newImage = builder.build();
+                StepResultEvent result = new ImageCalculated(event.getStepNumber(), newImage);
+                publish(result);
+            }
+        } catch (ClassNotFoundException e) {
+            // TODO log
+        }
+    }
 }
