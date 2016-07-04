d4c75f490cefee2be2e91aa105725844671b83e6
Damian Krawiec <dbgbkr@gmail.com>
2016-07-04 20:28:15 +0200

    [#28] Replace Mockito with test dedicated eventbus implementation

--- plugins/lookup-table/src/test/java/pl/edu/uj/kimage/plugins/lookup/table/InvertFlowStepTest.java
+++ plugins/lookup-table/src/test/java/pl/edu/uj/kimage/plugins/lookup/table/InvertFlowStepTest.java
@@ -2,10 +2,9 @@ package pl.edu.uj.kimage.plugins.lookup.table;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import pl.edu.uj.kimage.api.Step;
 import pl.edu.uj.kimage.api.StepDependency;
-import pl.edu.uj.kimage.eventbus.EventBus;
+import pl.edu.uj.kimage.eventbus.TestEventBus;
 import pl.edu.uj.kimage.plugin.ImageCalculated;
 import pl.edu.uj.kimage.plugin.StepResultEvent;
 import pl.edu.uj.kimage.plugin.model.Color;
@@ -14,14 +13,14 @@ import pl.edu.uj.kimage.plugin.model.ImageBuilder;
 
 import java.util.ArrayList;
 
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class InvertFlowStepTest {
     private static final int ONE_PIXEL = 1;
     private static final int STEP_NUMBER = 1;
     private static final int Y_COORDINATE = 0;
     private static final int X_COORDINATE = 0;
-    private EventBus eventBus;
+    private TestEventBus eventBus;
     private Image whiteImage;
     private Image blackImage;
 
@@ -29,7 +28,7 @@ public class InvertFlowStepTest {
     public void setUpd() {
         blackImage = new ImageBuilder(ONE_PIXEL, ONE_PIXEL).withColor(Color.BLACK, X_COORDINATE, Y_COORDINATE).build();
         whiteImage = new ImageBuilder(ONE_PIXEL, ONE_PIXEL).withColor(Color.WHITE, X_COORDINATE, Y_COORDINATE).build();
-        eventBus = mock(EventBus.class);
+        eventBus = new TestEventBus();
     }
 
     @Test
@@ -47,6 +46,6 @@ public class InvertFlowStepTest {
 
         // then
         StepResultEvent expectedResult = new ImageCalculated(1, whiteImage);
-        Mockito.verify(eventBus).publish(expectedResult);
+        assertThat(eventBus.getPublishedEvents()).contains(expectedResult);
     }
 }
